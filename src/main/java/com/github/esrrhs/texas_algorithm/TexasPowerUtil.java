94539ca48eff09d0114d4dea6eed7673e2661a80
esrrhs <esrrhs@163.com>
2018-07-19 16:21:20 +0800

    add

--- src/main/java/com/github/esrrhs/texas_algorithm/TexasPowerUtil.java
+++ src/main/java/com/github/esrrhs/texas_algorithm/TexasPowerUtil.java
@@ -1,25 +1,36 @@
 package com.github.esrrhs.texas_algorithm;
 
+import java.io.File;
+
 public class TexasPowerUtil
 {
 	public static void main(String[] args)
 	{
+		gen();
 		genOpt();
 	}
 
 	private static void gen()
 	{
-		GenUtil.genKey();
-		GenUtil.outputData();
+		File file = new File("texas_data.txt");
+		if (!file.exists())
+		{
+			GenUtil.genKey();
+			GenUtil.outputData();
+		}
 	}
 
 	private static void genOpt()
 	{
-		for (int i = 2; i <= 7; i++)
+		File file = new File("texas_data.txt");
+		if (file.exists())
 		{
-			GenOptUtil.N = i;
-			GenOptUtil.genKey();
-			GenOptUtil.transData();
+			for (int i = 2; i <= 7; i++)
+			{
+				GenOptUtil.N = i;
+				GenOptUtil.genKey();
+				GenOptUtil.transData();
+			}
 		}
 	}
 }
