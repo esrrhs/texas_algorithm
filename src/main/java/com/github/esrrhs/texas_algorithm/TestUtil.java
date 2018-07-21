6e7a7d4ae03393ae8708fc50f0061701bff2159a
bjzhaoxin <bjzhaoxin@corp.netease.com>
2018-07-21 15:54:25 +0800

    add

--- src/main/java/com/github/esrrhs/texas_algorithm/TestUtil.java
+++ src/main/java/com/github/esrrhs/texas_algorithm/TestUtil.java
@@ -5,7 +5,7 @@ public class TestUtil
 	public static void main(String[] args)
 	{
 		TexasAlgorithmUtil.load();
-		String cards = "♦6,♣3,♠2,♠3,♠4,♠5,♠6";
+		String cards = "方6,方6,黑2,黑3,黑4,黑5,黑6";
 		System.out.println(TexasAlgorithmUtil.getWinPosition(cards));
 		System.out.println(TexasAlgorithmUtil.getWinProbability(cards));
 		System.out.println(TexasAlgorithmUtil.keyToStr(TexasAlgorithmUtil.getWinMax(cards)));
